53e02cce14cc4fc521f83c0e9afef89a1f260810
Brian Francoeur <francoeurbj@gmail.com>
2020-03-02 17:19:50 -0500

    convert base js into class

--- carousel.js
+++ carousel.js
@@ -1,58 +1,110 @@
-  let slides = document.querySelectorAll('.slide');
-  let dots = document.querySelectorAll('.dot');
-  let currentSlide = 0;
 
-   let slidesLength = slides.length;
-   let dotsLength = 0;
-   slideInterval = setInterval(nextSlide, 5000);
+class Carousel {
+	constructor(carousel, slides, logos, dots, slidesLength, logosLength, dotsLength, currentSlide) {
+		this.carousel = document.getElementById('carousel');
+		// this.cookieManager = new CookieManager();
+		// this.slides = document.querySelectorAll( '.carousel__content' );
+		this.logos = document.querySelectorAll( '.logo' );
+		this.dots = document.querySelectorAll( '.dot' );
+		// this.slidesLength;
+		this.logosLength;
+		// this.dotsLength;
+    this.currentSlide = 0;
 
-   // function setSlideTime(slideInterval) {
-   //   if (nextSlide) {
-   //     slideInterval = setInterval(nextSlide, 5000);
-   //   } else {
-   //     slideInterval = setInterval(selectSlide, 5000);
-   //   }
-   // }
+    this.init();
+	}
+
+	init() {
+		if ( ! this.carousel ) {
+			return;
+		}
+
+		// this.carousel.forEach( ( carousel ) => {
+		// 	const content = carousel.querySelector( '.carousel__content' );
+		// 	if ( ! content ) {
+		// 		return;
+		// 	}
+		//
+		// 	const carouselId = content.innerHTML.replace( /[^\w]/gi, '' );
+		// 	const cookie = this.cookieManager.getCookie( carouselId );
+		// 	if ( 'dismissed' === cookie ) {
+		// 		return;
+		// 	}
+		// } );
+	}
 
 
-  function addDots() {
 
-    let carouselNav = document.getElementById('carousel-nav');
-    let span = document.createElement('span');
+	// dynamically add dots to carousel that equals the number of slides
+	addDots() {
+		this.slides = document.querySelectorAll( '.carousel__content' );
+		this.dots = document.querySelectorAll( '.dot' );
 
-    span.setAttribute('class', 'dot');
-    span.setAttribute('onclick', 'selectSlide(' + (dotsLength + 1) + ')');
+		this.slidesLength = this.slides.length;
+		this.dotsLength = 0;
 
-    carouselNav.appendChild(span);
+		const carouselNav = document.getElementById( 'carousel-nav' ),
+			span = document.createElement( 'span' );
 
-    dotsLength++;
+		while ( this.dotsLength < this.slidesLength ) {
+			span.setAttribute( 'class', 'dot' );
+			span.setAttribute( 'onclick', 'selectSlide( ' + ( this.dotsLength + 1 ) + ' )' );
 
-  }
+			carouselNav.appendChild( span );
 
-  while ( dotsLength < slidesLength ) {
-    addDots();
-  }
+			this.dotsLength++;
+			return this.dotsLength;
+		}
+	}
+
+	// select slide from nav dots
+	selectSlide( n, slides, logos, dots, currentSlide ) {
+		this.slides = document.querySelectorAll( '.carousel__content' );
+	  this.logos = document.querySelectorAll( '.logo' );
+		this.dots = document.querySelectorAll( '.dot' );
+
+		this.addDots(this.slides.length);
 
 
-function selectSlide(n) {
-    slides[currentSlide].classList.remove('showing');
-    dots[currentSlide].classList.remove('active');
 
-    currentSlide = (currentSlide = n - 1)%slides.length;
+		// this.addDots(slides.length);
+    console.log('this.slides: ' + this.slides);
+		console.log('selectSlide: ' + selectSlide); // currentSlide is undefined
+		this.slides[ selectSlide ].classList.remove( 'carousel--show' );
+		this.logos[ selectSlide ].classList.remove( 'active' );
+		this.dots[ selectSlide ].classList.remove( 'active' );
 
-    slides[currentSlide].classList.add('showing');
-    dots[currentSlide].classList.add('active');
+		this.currentSlide = ( currentSlide + 1 ) % slides.length;
+
+		this.slides[ selectSlide ].classList.add( 'carousel--show' );
+		this.logos[ selectSlide ].classList.add( 'active' );
+		this.dots[ selectSlide ].classList.add( 'active' );
+
+		setInterval( this.nextSlide(), 2000 );
+	}
+
+	nextSlide( n, slides, logos, dots, currentSlide ) {
+		this.slides = document.querySelectorAll( '.carousel__content' );
+	  this.logos = document.querySelectorAll( '.logo' );
+		this.dots = document.querySelectorAll( '.dot' );
+
+		this.addDots(this.slides.length);
+
+    console.log('this.slides: ' + this.slides);
+		console.log('selectSlide: ' + selectSlide); // currentSlide is undefined
+		this.slides[ selectSlide ].classList.remove( 'carousel--show' );
+		this.logos[ selectSlide ].classList.remove( 'active' );
+		this.dots[ selectSlide ].classList.remove( 'active' );
+
+		this.currentSlide = ( currentSlide + 1 ) % slides.length;
+
+		this.slides[ selectSlide ].classList.add( 'carousel--show' );
+		this.logos[ selectSlide ].classList.add( 'active' );
+		this.dots[ selectSlide ].classList.add( 'active' );
+
+		setInterval( this.nextSlide(), 2000 );
+	}
 }
 
-function nextSlide() {
-    slides = document.querySelectorAll('.slide');
-    dots = document.querySelectorAll('.dot');
-
-    slides[currentSlide].classList.remove('showing');
-    dots[currentSlide].classList.remove('active');
-
-    currentSlide = (currentSlide+1)%slides.length;
-
-    slides[currentSlide].classList.add('showing');
-    dots[currentSlide].classList.add('active');
-}
+let carousel = new Carousel();
+carousel.nextSlide();
